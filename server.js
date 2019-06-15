440d320fd1d079eff1c99f806188fa0963d9c527
Christa <christahyde@yahoo.com>
2019-06-15 13:54:19 -0600

    changed port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000 || process.env;
+var PORT = 3000 || process.env.port;
 
 // Initialize Express
 var app = express();
